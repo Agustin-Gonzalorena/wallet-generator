0088ce941432ef827b6cdabf631d07216e716cd3
Agustin-Gonzalorena <agonzalorena1@gmail.com>
2024-12-15 22:55:20 -0300

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7048144468201725"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body
         id="body"
         className={`${dm_sans.className} flex justify-center w-full m-0`}
